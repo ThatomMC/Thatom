1667409e3a3735a799bacfc3fe880d2c5093d87c
Pronner <pronnerdev@gmail.com>
2022-04-04 19:19:52 +0400

    definitions added (#1)
    definitions added (#1)

---
+++ src/definitions/thatom.cs
@@ -0,0 +1,14 @@
+using Thatom;
+
+
+private enum THATOM_SDK
+{
+   THATOM_ACCESSVALID,
+   THATOM_ACCESSINVALID,
+   THATOM_MCBE,
+   THATOM_MCPE,
+   THATOM_USERINFO,
+   THATOM_EDIT
+}
+
+// These are current definitions for your server core/plugins. Our documentation will explain this to you.
